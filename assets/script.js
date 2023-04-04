d04b552cfa48d579442834f6982eb194750ea829
PhilippWinston <philippwinston@gmail.com>
2023-04-03 20:49:03 -0500

    added script tags and the first Marvel API call

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,44 @@
 var MarvelAPIKey = '56d680ad350989db0b995c28d74c5a40'
+var MarvelPrivateAPIKey = '04b3f0728e999a4bde3264d4bc7453585ed435d7'
 var GIFHYAPIKey = '43tnZfZ4jipkYDI9CLL5Vxf8VrJQ6daP'
+
+var characterName = 'Thor';
+
+const timestamp = new Date().getTime().toString();
+console.log(Date)
+
+var hash = md5(timestamp + MarvelPrivateAPIKey + MarvelAPIKey)
+var marvelQueryURL = `https://gateway.marvel.com:443/v1/public/characters?name=${characterName}&apikey=${MarvelAPIKey}&ts=${timestamp}&hash=${hash}`;
+
+fetch(marvelQueryURL)
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (data) {
+    const results = data.data.results;
+    let characterInfo = '';
+    results.forEach(function (result) {
+      characterInfo += `Name: ${result.name}\n`;
+      characterInfo += `Description: ${result.description}\n`
+      characterInfo += `thumbnail: ${result.thumbnail}\n`;;
+    });
+    console.log(characterInfo);
+  })
+  .catch(function (error) {
+    console.error(error);
+  });
+  
+  
+
+
+
+
+
+
+
+
+
+
 
 
 document.getElementById("img1").addEventListener("click", function () {
