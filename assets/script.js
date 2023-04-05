3d9673a78d6effb5e5a7256ed57f0f7b0d2152b5
PhilippWinston <philippwinston@gmail.com>
2023-04-04 19:54:05 -0500

    added to the JS, got the gifhy api to pull

--- assets/script.js
+++ assets/script.js
@@ -1,13 +1,13 @@
-var MarvelAPIKey = '56d680ad350989db0b995c28d74c5a40'
-var MarvelPrivateAPIKey = '04b3f0728e999a4bde3264d4bc7453585ed435d7'
-var GIFHYAPIKey = '43tnZfZ4jipkYDI9CLL5Vxf8VrJQ6daP'
+var MarvelAPIKey = "56d680ad350989db0b995c28d74c5a40";
+var MarvelPrivateAPIKey = "04b3f0728e999a4bde3264d4bc7453585ed435d7";
+var GIFHYAPIKey = "43tnZfZ4jipkYDI9CLL5Vxf8VrJQ6daP";
 
-var characterName = 'Thor';
+var characterName = "Thor";
 
 const timestamp = new Date().getTime().toString();
-console.log(Date)
+console.log(Date);
 
-var hash = md5(timestamp + MarvelPrivateAPIKey + MarvelAPIKey)
+var hash = md5(timestamp + MarvelPrivateAPIKey + MarvelAPIKey);
 var marvelQueryURL = `https://gateway.marvel.com:443/v1/public/characters?name=${characterName}&apikey=${MarvelAPIKey}&ts=${timestamp}&hash=${hash}`;
 
 fetch(marvelQueryURL)
@@ -16,47 +16,54 @@ fetch(marvelQueryURL)
   })
   .then(function (data) {
     const results = data.data.results;
-    let characterInfo = '';
+    let characterInfo = "";
     results.forEach(function (result) {
       characterInfo += `Name: ${result.name}\n`;
-      characterInfo += `Description: ${result.description}\n`
+      characterInfo += `Description: ${result.description}\n`;
       characterInfo += `thumbnail: ${result.thumbnail}\n`;
 
-      document.querySelector('.description').textContent = result.description
-      document.querySelector('.name').textContent = result.name
-      document.querySelector('.thumbnail').innerHTML = `<img src="${result.thumbnail.path}.${result.thumbnail.extension}" alt="${result.name} thumbnail" />`
+      document.querySelector(".description").textContent = result.description;
+      document.querySelector(".name").textContent = result.name;
+      document.querySelector(
+        ".thumbnail"
+      ).innerHTML = `<img src="${result.thumbnail.path}.${result.thumbnail.extension}" alt="${result.name} thumbnail" />`;
     });
     console.log(characterInfo);
 
     // Save the data to local storage
-    localStorage.setItem('marvelData', JSON.stringify(data));
+    localStorage.setItem("marvelData", JSON.stringify(data));
   })
   .catch(function (error) {
     console.error(error);
   });
-  
-  
 
+var searchTerm = "Thor";
 
-
-
-
-
-
-
-
-
-
+var giphyQueryURL = `https://api.giphy.com/v1/gifs/random?api_key=${GIFHYAPIKey}&tag=${searchTerm}&rating=pg-13`;
+fetch(giphyQueryURL)
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (data) {
+    console.log(data);
+    var gifUrl = data.data.images.original.webp;
+    document.getElementById(
+      "gif"
+    ).innerHTML = `<img src="${gifUrl}" alt="${searchTerm} gif" />`;
+  })
+  .catch(function (error) {
+    console.error(error);
+  });
 
 document.getElementById("img1").addEventListener("click", function () {
-    // API call goes here
-    fetch("")
-      .then((Response) => Response.json())
-      .then((data) => {
-        document.getElementById("description").innerHTML = data.Response;
-      });
-      console.log(img1)
-  });
+  // API call goes here
+  fetch("")
+    .then((Response) => Response.json())
+    .then((data) => {
+      document.getElementById("description").innerHTML = data.Response;
+    });
+  console.log(img1);
+});
 document.getElementById("img2").addEventListener("click", function () {
   // API call goes here
   fetch(" ")
@@ -66,26 +73,26 @@ document.getElementById("img2").addEventListener("click", function () {
     });
 });
 document.getElementById("img3").addEventListener("click", function () {
-    // API call goes here
-    fetch(" ")
-      .then((Response) => Response.json())
-      .then((data) => {
-        document.getElementById("img3").innerHTML = data.Response;
-      });
-  });
-  document.getElementById("img4").addEventListener("click", function () {
-    // API call goes here
-    fetch(" ")
-      .then((Response) => Response.json())
-      .then((data) => {
-        document.getElementById("img4").innerHTML = data.Response;
-      });
-  });
-  document.getElementById("img5").addEventListener("click", function () {
-    // API call goes here
-    fetch(" ")
-      .then((Response) => Response.json())
-      .then((data) => {
-        document.getElementById("img5").innerHTML = data.Response;
-      });
-  });
+  // API call goes here
+  fetch(" ")
+    .then((Response) => Response.json())
+    .then((data) => {
+      document.getElementById("img3").innerHTML = data.Response;
+    });
+});
+document.getElementById("img4").addEventListener("click", function () {
+  // API call goes here
+  fetch(" ")
+    .then((Response) => Response.json())
+    .then((data) => {
+      document.getElementById("img4").innerHTML = data.Response;
+    });
+});
+document.getElementById("img5").addEventListener("click", function () {
+  // API call goes here
+  fetch(" ")
+    .then((Response) => Response.json())
+    .then((data) => {
+      document.getElementById("img5").innerHTML = data.Response;
+    });
+});
